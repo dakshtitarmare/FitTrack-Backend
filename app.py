80741efc0df29c48b3c235c9d4b4ee76fea4d0e6
daksh <dakshtitarmare2006@gmail.com>
2025-09-16 23:16:24 +0530

    update-ip

--- app.py
+++ app.py
@@ -76,5 +76,5 @@ def get_qr_data(uid):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))  # fallback to 5000 for local
     app.run(host="0.0.0.0", port=port)
